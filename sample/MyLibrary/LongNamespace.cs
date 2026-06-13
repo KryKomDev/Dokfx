7c7aa784f763144bb35a1769d57acad88682f613
KryKomDev <krystof.komzak@gmail.com>
2026-06-13 20:49:12 +0200

    * fix link text wrapping in left navbar, * hide expander icon when all buttons are expanded to the full width

---
+++ sample/MyLibrary/LongNamespace.cs
@@ -0,0 +1,10 @@
+namespace MyLibrary.AVeryVery.LongNamespace
+{
+    public class MyClass
+    {
+        public void MyMethod()
+        {
+            // Method implementation
+        }
+    }
+}
